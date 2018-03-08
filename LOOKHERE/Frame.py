58b5297249fff6d32466601919516049204195fa
Shiksx <Shiksx@yandex.ru>
2018-03-08 12:24:54 +0500

    ;gk;

--- LOOKHERE/Frame.py
+++ LOOKHERE/Frame.py
@@ -12,11 +12,12 @@ import CarSettings
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 
-
-camera = PiCamera()
-camera.resolution = (CarSettings.PiCameraResW, CarSettings.PiCameraResH)
-camera.framerate = CarSettings.PiCameraFrameRate
-camera.vflip = True
-camera.hflip = True
-rawCapture = PiRGBArray(camera, size=(CarSettings.PiCameraResW, CarSettings.PiCameraResH))
-print (camera.capture_continuous(rawCapture, format="bgr", use_video_port=True)[0])
+camera = cv2.VideoCapture(0)
+#camera.resolution = (CarSettings.PiCameraResW, CarSettings.PiCameraResH)
+#camera.framerate = CarSettings.PiCameraFrameRate
+#camera.vflip = True
+#camera.hflip = True
+#rawCapture = PiRGBArray(camera, size=(CarSettings.PiCameraResW, CarSettings.PiCameraResH))
+image = camera.read()
+image = cv2.flip(image, -1)
+print (image)
